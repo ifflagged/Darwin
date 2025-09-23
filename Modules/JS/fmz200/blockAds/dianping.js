d58d8797754eb65d541674f848b5182e82eaa1f1
ifflagged <ifflagged@users.noreply.github.com>
2025-09-23 06:19:55 +0000

    Converted modules at 2025-09-23 14:19:55 (UTC+8)

--- Modules/JS/fmz200/blockAds/dianping.js
+++ Modules/JS/fmz200/blockAds/dianping.js
@@ -16,8 +16,10 @@ if (url.includes("/dpmobile") || url.includes("/goodsawardpic")) {
   const traceKey2 = Object.keys(header).find(key => /^(ai|dt|al|u)$/i.test(key));
   const headopt1 = traceKey1 ? header[traceKey1] : null;
   const headopt2 = traceKey2 ? header[traceKey2] : null;
-  if ((headopt1 && !headopt2) || url.includes(".gif.webp")) {
+  if (headopt1 && !headopt2) {
     $done({body: "", headers: "", status: "HTTP/1.1 404 Not Found"});
+  } else if (url.includes(".gif.webp")) {
+    $done({bodyBytes: new ArrayBuffer(8)});
   } else {
     $done({});
   }
