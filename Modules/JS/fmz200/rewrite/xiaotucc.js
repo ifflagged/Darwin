cad3335a976309610e0d097581a198c897bcb6df
ifflagged <ifflagged@users.noreply.github.com>
2026-07-09 01:16:23 +0000

    Converted modules at 2026-07-09 09:16:23 (UTC+8)

--- Modules/JS/fmz200/rewrite/xiaotucc.js
+++ Modules/JS/fmz200/rewrite/xiaotucc.js
@@ -1,9 +1,2 @@
-if (-1 != $request.url.indexOf("main_page/index/getActivity")) {
-  let obj = JSON.parse($response.body);
-  delete obj.data.p3;
-  $done({body: JSON.stringify(obj)});
-} else if (-1 != $request.url.indexOf("mall/main")) {
-  let obj = JSON.parse($response.body);
-  delete obj.data;
-  $done({body: JSON.stringify(obj)});
-} else $done($response);
+429: Too Many Requests
+For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
--- Modules/JS/fmz200/rewrite/xmApp.js
+++ Modules/JS/fmz200/rewrite/xmApp.js
@@ -1,2 +1,13 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+let url = $request.url;
+let Body = JSON.parse($response.body);
+if (url.match(/icon/)) {
+    Body.data = {
+        ...Body.data,
+        big_promotion_center: {},
+        active_center: {}
+    }
+} else if(url.match(/index/)){
+    let filtData=Body.data.filter(e=>e.hasMore);
+    Body.data = filtData;
+}
+$done({ body: JSON.stringify(Body) })
