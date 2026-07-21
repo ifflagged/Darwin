25074beec9784715a239eede7e03eade19d10990
ifflagged <ifflagged@users.noreply.github.com>
2026-07-21 20:45:43 +0000

    Converted modules at 2026-07-22 04:45:42 (UTC+8)

--- Modules/JS/fmz200/blockAds/fly.js
+++ Modules/JS/fmz200/blockAds/fly.js
@@ -1,13 +1,17 @@
-var body = $response.body;
-var new_body = JSON.parse(body);
 
-if (new_body.Variables.data.threaddetail) {
-  new_body.Variables.data.threaddetail.tagadv = "";
-  new_body.Variables.data.threaddetail.threadapp_ad_video = [];
-  new_body.Variables.data.threaddetail.pingyouadv = "";
-  new_body.Variables.data.threaddetail.middleadv = "";
-  new_body.Variables.data.threaddetail.bottomadv = "";
-  new_body.Variables.data.threaddetail.appdetailadv = [];
-}
-
-$done({body: JSON.stringify(new_body)});
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>503 first byte timeout</title>
+  </head>
+  <body>
+    <h1>Error 503 first byte timeout</h1>
+    <p>first byte timeout</p>
+    <h3>Error 54113</h3>
+    <p>Details: cache-chi-kigq8000084-CHI 1784666619 4013586382</p>
+    <hr>
+    <p>Varnish cache server</p>
+  </body>
+</html>
